bf03511b436fbd08bb0b1719887bf80d6be2fab1
BychaBykov <128513251+BychaBykov@users.noreply.github.com>
2023-03-23 15:37:42 +0700

    Add files via upload

---
+++ Структуры_пз2/Структуры пз2/DeckLib.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+
+struct queue_1 {
+	int beg = 0;
+	int end = -1;
+	int data[20];
+};
+
+struct queue_2 {
+	int beg = 0;
+	int end = -1;
+	int data[20][2];
+};
+
+int push(queue_1* q, int a) {
+	if (((q->end + 1) % 20 == q->beg) && (q->end != -1))
+		return 0;
+	int i = (q->end + 1) % 20;
+	while ((i != q->beg) && (q->data[(i - 1) % 20] > a)) {
+		q->data[i] = q->data[(i - 1) % 20];
+		i = (i - 1) % 20;
+	}
+	q->data[i] = a;
+	q->end = (q->end + 1) % 20;
+	return 1;
+}
+
+int pushM(queue_2* q, int k, int j) {
+	if (((q->end + 1) % 20 == q->beg) && (q->end != -1))
+		return 0;
+	if (q->end == -1) {
+		q->end++;
+		q->data[q->end][0] = k;
+		q->data[q->end][1] = j;
+		return 1;
+	}
+	int i = (q->end + 1) % 20;
+	while ((i != q->beg) && (q->data[(i - 1) % 20][1] > j)) {
+		q->data[i][0] = q->data[(i - 1) % 20][0];
+		q->data[i][1] = q->data[(i - 1) % 20][1];
+		i = (i - 1) % 20;
+	}
+	q->data[i][0] = k;
+	q->data[i][1] = j;
+	q->end = (q->end + 1) % 20;
+	return 1;
+}
+
+int pushP(queue_2* q, int k, int j) {
+	if (((q->end + 1) % 20 == q->beg) && (q->end != -1))
+		return 0;
+	if (q->end == -1) {
+		q->end++;
+		q->data[q->end][0] = k;
+		q->data[q->end][1] = j;
+		return 1;
+	}
+	int i = (q->end + 1) % 20;
+	while ((i != q->beg) && (q->data[(i - 1) % 20][0] > k)) {
+		q->data[i][0] = q->data[(i - 1) % 20][0];
+		q->data[i][1] = q->data[(i - 1) % 20][1];
+		i = (i - 1) % 20;
+	}
+	q->data[i][0] = k;
+	q->data[i][1] = j;
+	q->end = (q->end + 1) % 20;
+	return 1;
+}
+
+int pop(queue_1 *q, int* a)
+{
+	if (q->end == -1)
+		return 0;
+	*a = q->data[q->beg];
+	q->data[q->beg] = -1;
+	if (q->beg == q->end) {
+		q->beg = 0;
+		q->end = -1;
+		return 1;
+	}
+	else
+		q->beg = (q->beg + 1) % 20;
+	return 1;
+}
+
+int popM(queue_2* q, int* a) {
+	if (q->end == -1)
+		return 0;
+	a[0] = q->data[q->beg][0];
+	a[1] = q->data[q->beg][1];
+	if (q->beg == q->end) {
+		q->beg = 0;
+		q->end = -1;
+		return 1;
+	}
+	else
+		q->beg = (q->beg + 1) % 20;
+	
+	return 1;
+}
+
+
---
+++ Структуры_пз2/Структуры пз2/Структуры пз2.cpp
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "DeckLib.cpp"
+
+
+FILE* myfile;
+
+void program(queue_1** A, queue_2** B, queue_2** C, char a, int *j) {
+	if (a == '(') {
+		j++;
+		push(*A, *j);
+	}
+	else if (a == ')') {
+		int k;
+		pop(*A, &k);
+		j++;
+		pushP(*B, k, *j);
+		pushM(*C, k, *j);
+	}
+}
+
+void vyvod(queue_2* B, queue_2* C) {
+	myfile = fopen("output.txt", "w");
+	int a = 0;
+	while (a <= B->end) {
+		fprintf_s(myfile, "(%i %i) ", B->data[a][0], B->data[a][1]);
+		a++;
+	}
+	a = 0;
+	while (a <= C->end) {
+		fprintf_s(myfile, "(%i %i) ", C->data[a][0], C->data[a][1]);
+		a++;
+	}
+}
+
+int main() {
+	myfile = fopen("input.txt", "r");
+	if (myfile == NULL){
+		myfile = fopen("output.txt", "w");
+		fprintf_s(myfile, " ");
+	}
+	else {
+		queue_1* A = new queue_1;
+		queue_2* B = new queue_2;
+		queue_2* C = new queue_2;
+		char sym;
+		int j = 0;
+		while (fscanf(myfile, "%c", &sym) != EOF) {
+			program(&A, &B, &C,sym,&j);
+		}
+	}
+	int afafa = 0;
+	return 0;
+}
+
