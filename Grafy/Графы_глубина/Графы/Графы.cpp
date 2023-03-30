5fe88521746e5fc7bb51f7a9b7cbdf5a88915bf0
BychaBykov <bbykov1112@gmail.com>
2023-03-30 22:55:30 +0700

    grafy

---
+++ Grafy/Графы_глубина/Графы/Графы.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+struct Graf {
+	vector<vector<int>> graf;
+};
+
+void input(Graf& graf, int& v);
+int DFS(Graf graf, int v);
+
+int main()
+{
+	Graf graf;
+	int v;
+	input(graf,v);
+	
+}
+
+
+void input(Graf& graf, int& v) {
+	ifstream file("graf.txt");
+	int n, m;
+	file >> n >> m >> v;
+	graf.graf.resize(n, vector<int>(n));
+	int a, b;
+	for (int i = 0; i < m; i++) {
+		file >> a >> b;
+		graf.graf[a][b] = 1;
+	}
+	file.close();
+}
+
+int DFS(Graf graf, int v) {
+	int i = v;
+
+}
