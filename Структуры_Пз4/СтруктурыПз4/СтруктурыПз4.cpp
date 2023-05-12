6263326f929898daa7fac75f1c54363a96b3d0ef
BychaBykov <bbykov1112@gmail.com>
2023-05-12 12:42:53 +0700

    pz5fin

---
+++ Структуры_Пз4/СтруктурыПз4/СтруктурыПз4.cpp
@@ -0,0 +1,122 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+const int N = 19;
+FILE* myfile;
+
+struct elem {
+	char name[256];
+	char type[15];
+	int mem;
+	int k;
+	char type_k[15];
+	int DELETE;
+};
+
+struct types {
+	char D[7];
+	int size;
+};
+
+int h(char* str) {
+	int len = strlen(str);
+	int ans = 0;
+	for (int i = 0; i < len; i++)
+		ans += int(str[i]);
+	return (ans % N);
+}
+
+void elemdel(elem* t, char* key) {
+	int k = h(key);
+	if (strcmp(t[k % N].name, "") == 0 && !t[k].DELETE);
+	else {
+		int counter = 0;
+		while (strcmp(t[k % N].name, key) != 0 && (strcmp(t[k % N].name, "") != 0 || t[k].DELETE == 1) && counter != N) {
+			k = (k + 1) % N;
+		}
+		if (!strcmp(t[k % N].name, key)) {
+			strcpy_s(t[k].name, "");
+			t[k].DELETE = 1;
+		}
+	}
+}
+
+int find(int k, elem* t) {
+	int counter = 0;
+	while (strcmp(t[k % N].name, "")) {
+		k = (k + 1)%N;
+		counter += 1;
+		if (counter == N) {
+			return -1;
+		}
+	}
+	return k;
+}
+
+void vyvod(elem* t) {
+	myfile = fopen("table.txt", "w");
+	for (int i = 0; i < N; i++) {
+		if(strcmp(t[i].name, ""))
+			fprintf(myfile, "%i %s %s %i %i %s\n",i, t[i].name, t[i].type, t[i].mem, t[i].k, t[i].type_k);
+	}
+}
+
+void input(elem* t) {
+	myfile = fopen("input.txt", "r");
+	char* str;
+	char name[256];
+	char type[7];
+	int mem;
+	int k;
+	int key = 0;
+	char type_k[7];
+	char a[250];
+	types data[4];
+	strcpy_s(data[0].D, "int");
+	data[0].size = sizeof(int);
+	strcpy_s(data[1].D, "float");
+	data[1].size = sizeof(float);
+	strcpy_s(data[2].D, "double");
+	data[2].size = sizeof(double);
+	strcpy_s(data[3].D, "bool");
+	data[3].size = sizeof(bool);
+	fscanf_s(myfile, "%s", a, sizeof(a));
+	while (strcmp(a, "{"))
+		fscanf_s(myfile, "%s", a, sizeof(a));
+	while (fscanf(myfile, "%s", a) != EOF && key != -1) {
+		for (int i = 0; i < 4; i++) {
+			if (!strcmp(a, data[i].D)) {
+				strcpy_s(type, a);
+				fscanf_s(myfile, " %[^[;]", a, sizeof(a));
+				key = h(a);
+				key = find(key, t);
+				if (key != -1) {
+					strcpy_s(name, a);
+					t[key].mem = data[i].size;
+					strcpy_s(t[key].name, name);
+					strcpy_s(t[key].type, type);
+					if (fgetc(myfile) == '[') {
+						fscanf_s(myfile, "%d", &k);
+						t[key].k = k;
+						strcpy_s(t[key].type, "array");
+						strcpy_s(t[key].type_k, type);
+					}
+				}
+			}
+		}
+	}
+};
+
+int main()
+{
+	elem t[N];
+	for (int i = 0; i < N; i++) {
+		strcpy_s(t[i].name, "");
+		t[i].k = 0;
+		strcpy_s(t[i].type_k, "NONE");
+		t[i].DELETE = 0;
+	}
+	input(t);
+	vyvod(t);
+}
--- Структуры_пз5/Структуры_пз5/Структуры_пз5.cpp
+++ Структуры_пз5/Структуры_пз5/Структуры_пз5.cpp
@@ -11,7 +11,7 @@ struct elem {
 	int mem;
 	int k;
 	char type_k[15];
-	int num = -1;
+	int num = 0;
 };
 
 struct types {
@@ -19,16 +19,101 @@ struct types {
 	int size;
 };
 
+int h(char* a) {
+	int len = strlen(a);
+	int ans = 0;
+	for (int i = 0; i < len; i++)
+		ans += int(a[i]);
+	return (ans % N);
+}
+
+void add(elem* t, char* a) {
+	int k = h(a);
+	int counter = 0;
+	while (strcmp(t[k%N].name, a) && counter!= N) {
+		k +=1;
+		counter += 1;
+	}
+	if (!strcmp(t[k % N].name, a)) {
+		t[k%N].num += 1;
+	}
+}
+
 void input(elem* t){
 	myfile = fopen("table.txt", "r");
-	for (int i = 0; i < N; i++) {
-		fscanf(myfile, "%s %s %i %i %s", t[i].name, t[i].type, &t[i].mem, &t[i].k, t[i].type_k);
+	int i = 0;
+	while (fscanf(myfile, "%i ", &i) != EOF) {
+		fscanf(myfile, "%s %s %i %i %s", t[i].name, t[i].type, &t[i].mem, &t[i].k,t[i].type_k);
+	}
+	myfile = fopen("code.txt", "r");
+	char a[25];
+	char prev[25];
+	while (fscanf_s(myfile, "%s ", a,sizeof(a)) != EOF) {
+		if (!strcmp(a, "=")) {
+			add(t, prev);
+			while (fscanf_s(myfile, " %[^[; ] ", a, sizeof(a)) != 0) {
+				if (strcmp(a, "+")) add(t, a);
+			}
+		}
+		else if (!strcmp(a, "if(")) {
+			while (fscanf_s(myfile, " %[^[) ]", a, sizeof(a)) != 0) {
+				add(t, a);
+			}
+		}
+		strcpy_s(prev, a);
+	}
+}
+
+void Delete(elem* t, int L, int R, int* ri, int* rj)
+{
+	elem c = t[(L+R)/2];
+	int i = L;
+	int j = R;
+	while (i <= j)
+	{
+		while (t[i].num > c.num) i++;
+		while (t[j].num < c.num) j--;
+		if (i <= j)
+		{
+			elem x = t[i];
+			t[i] = t[j];
+			t[j] = x;
+			i++;
+			j--;
+		}
+	}
+	*ri = i;
+	*rj = j;
+}
+void Quicksort(elem* t, int L, int R)
+{
+	int i;
+	int j;
+	if (R - L > 0)
+	{
+		Delete(t, L, R, &i, &j);
+		if (L < j) Quicksort(t, L, j);
+		if (i < R) Quicksort(t, i, R);
+	}
+}
+
+void vyvod(elem* t) {
+	int i = 0;
+	myfile = fopen("output.txt", "w");
+	while( i < N) {
+		if (strcmp(t[i].name, ""))
+		fprintf(myfile, "%i %s %s %i %i %s %i\n", i, t[i].name, t[i].type, t[i].mem, t[i].k, t[i].type_k, t[i].num);
+		i++;
 	}
 }
 
 int main()
 {
 	elem t[N];
+	for (int i = 0; i < N; i++) {
+		strcpy_s(t[i].name, "");
+	}
 	input(t);
-
+    Quicksort(t, 0, N - 1);
+	vyvod(t);
 }
